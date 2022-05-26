498d825ac070fedb5985dd2a5e507d98106a7127
Patricia Fiol <patricia.fiol@outlook.com>
2022-05-26 15:48:56 +0200

    start registration form

---
+++ client/src/components/Register.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="register">
+    <h1>Welcome!</h1>
+    <input type="text" name="username" placeholder="name" /><br />
+    <input type="email" name="email" placeholder="email" /> <br />
+    <input type="password" name="password" placeholder="password" /> <br />
+    <button type="submit" name="submit">Register</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Register-User",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss"></style>
---
+++ client/src/services/authentication-service.js
@@ -0,0 +1,13 @@
+import api from "./api";
+
+export default {
+  register(credentials) {
+    return api().post('register', credentials)
+  }
+}
+
+// AuthenticationService.register({
+//   name: ''
+//   email: 'testing@gmail.com'
+//   password: ''
+// })
